86e595e0e28f2d7417ea4ed919f960c983092f06
Anisur Rahman <anisoncloud@gmail.com>
2026-07-14 00:43:56 +0600

    Initial Update

---
+++ HRM.Presentation/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HRM.Presentation.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ HRML.Application/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace HRML.Application
+{
+    public class Class1
+    {
+
+    }
+}
